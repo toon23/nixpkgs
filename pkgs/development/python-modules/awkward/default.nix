b265dd0d9d38d5797f2e088461b7c2881429c383
Frederik Rietdijk <fridh@fridh.nl>
2019-04-07 09:12:38 +0200

    python: awkward: 0.8.7 -> 0.8.14

--- pkgs/development/python-modules/awkward/default.nix
+++ pkgs/development/python-modules/awkward/default.nix
@@ -9,11 +9,11 @@
 
 buildPythonPackage rec {
   pname = "awkward";
-  version = "0.8.7";
+  version = "0.8.14";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "0a53c484za2l4yy1i05qhkylvygg8fnh4j1v3n35x2dsi929awdp";
+    sha256 = "28c8b79fee51b9b3a0281f8ab9d1f6958d3bf5b6a8b15e5884b4ad637b6bc8d5";
   };
 
   nativeBuildInputs = [ pytestrunner ];
